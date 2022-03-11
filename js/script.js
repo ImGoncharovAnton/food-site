834e4c35f253812b62383443f6faab3e7f63e60c
Anton Goncharov <gold-library@yandex.ru>
2022-03-12 00:00:26 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,30 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import {openModal} from './modules/modal';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 400000); // Вывод модального окна через время
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2022-04-11');
+    cards();
+    calc();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        slide: '.offer__slide',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner',
+    });
+});
+
---
+++ js/services/services.js
@@ -0,0 +1,25 @@
+const postData = async (url, data) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: data
+    });
+
+    return res.json();
+};
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Fetching ${url} failed, status: ${res.status}`);
+    }
+
+    return res.json();
+};
+
+
+export {postData};
+export {getResource};
